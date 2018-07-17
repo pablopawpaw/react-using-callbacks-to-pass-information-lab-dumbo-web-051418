8a09a581bcc6bbb188c66c6d2b233ee574bfa161
pablopawpaw <lpault29@gmail.com>
2018-07-17 11:33:58 -0400

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 
 export default class Cell extends Component {
-  
+
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
     }
   }
-  
+
+  changeColor = () => {
+    this.setState({
+      color: this.props.selectColor
+    })
+  }
+
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div className="cell" style={{backgroundColor: this.state.color}} onClick={this.changeColor}>
       </div>
     )
   }
-  
+
 }
--- src/Matrix.js
+++ src/Matrix.js
@@ -4,31 +4,37 @@ import Cell from './Cell.js'
 import ColorSelector from './ColorSelector.js'
 
 export default class Matrix extends Component {
-  
-  constructor() {
-    super()
+
+  constructor(props) {
+    super(props)
+    this.state = {selectedColor: '#000'}
+    this.onColorChange = this.onColorChange.bind(this)
   }
-  
+
   genRow = (vals) => (
-    vals.map(val => <Cell color={val} />)
+    vals.map(val => <Cell color={val} selectColor={this.state.selectedColor}/>)
   )
-  
+
   genMatrix = () => (
     this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   )
 
-  
+  onColorChange(event) {
+    this.setState({selectedColor: event.target.dataset.color})
+    console.log(this.state.selectedColor);
+  }
+
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector selectColor={this.onColorChange} />
         <div id="matrix">
           {this.genMatrix()}
         </div>
       </div>
     )
   }
-  
+
 }
 
 Matrix.defaultProps = {
